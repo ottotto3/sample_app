23b10b3372c335763ee59171323d2047d05f4c6c
EC2 Default User <ec2-user@ip-172-31-7-37.ap-northeast-1.compute.internal>
2024-02-08 05:51:45 +0000

    [Start]一覧画面表示

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,8 +1,20 @@
 class ListsController < ApplicationController
   def new
+    # Viewへ渡すためのインスタンス変数に空のmodelオブジェクトを生成する。
+    @list = List.new
   end
-
+  
+  def create
+    # 1.&2. データを受取,新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    #4. トップ画面へリダイレクト
+    redirect_to '/top'
+  end 
+  
   def index
+    @lists = List.all
   end
 
   def show
@@ -10,4 +22,10 @@ class ListsController < ApplicationController
 
   def edit
   end
+  
+  private
+  # ストロングパラメーター
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end 
 end
